e9faf8fbcfdc29a0c1adc851531c9d3f7ac98722
Ahmed Adewale <lilhamad8@gmail.com>
2025-03-13 01:41:30 +0100

    added message sending from landing page

--- js/ajax-mail.js
+++ js/ajax-mail.js
@@ -30,7 +30,7 @@
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
 			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				$(formMessages).text('Try to hit send from your mail app, winks...');
 			}
 		});
 	});
